82703b4873e34cc51338b547adcb82c17aa08a46
Sanninm <mudzelet.sanin@gmail.com>
2021-09-29 09:16:45 +0200

    ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,12 @@
+let firstName = (firstName) => {
+return firstName.toUpperCase();
+}
+
+let lastName = (lastName) => {
+    return lastName.toLowerCase();
+}
+
+exports.firstName = firstName;
+exports.lastName = lastName;
+
+
